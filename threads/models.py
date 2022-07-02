8aefacb5b2919d53347e80099da767de2d0b5de6
Wesley Barros <wesleybarros950@gmail.com>
2022-07-01 22:39:42 -0300

    Threads and comments now have a user

--- threads/models.py
+++ threads/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 from django.utils import timezone
 
@@ -6,6 +8,7 @@ class Thread(models.Model):
     text = models.CharField(max_length=512)
     pub_date = models.DateTimeField(default=timezone.now)
     points = models.IntegerField(default=0)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
@@ -14,6 +17,7 @@ class Comment(models.Model):
     text = models.CharField(max_length=256)
     pub_date = models.DateTimeField(default=timezone.now)
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.text
--- threads/views.py
+++ threads/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.urls import reverse
 
 from .models import Thread, Comment
@@ -14,21 +15,24 @@ def detail(request, thread_id):
     comments = Comment.objects.filter(thread=thread.id).order_by('-pub_date')
     return render(request, 'threads/detail.html', { 'thread': thread, 'comments': comments })
 
+@login_required
 def new(request):
     if request.method == 'POST':
-        new_thread = Thread(title=request.POST['thread-title'], text=request.POST['thread-text'])
+        new_thread = Thread(title=request.POST['thread-title'], text=request.POST['thread-text'], creator=request.user)
         new_thread.save()
         return HttpResponseRedirect(reverse('threads:detail', args=(new_thread.id, )))
     return render(request, 'threads/new.html')
 
+@login_required
 def upvote(request, thread_id):
     thread = get_object_or_404(Thread, id=thread_id)
     thread.points += 1
     thread.save()
     return HttpResponseRedirect(reverse('threads:detail', args=(thread.id, )))
 
+@login_required
 def comment(request, thread_id):
     thread = get_object_or_404(Thread, id=thread_id)
-    new_comment = Comment(text=request.POST['comment-text'], thread=thread)
+    new_comment = Comment(text=request.POST['comment-text'], thread=thread, creator=request.user)
     new_comment.save()
     return HttpResponseRedirect(reverse('threads:detail', args=(thread.id, )))
